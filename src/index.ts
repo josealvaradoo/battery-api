dc495880a262164d35a257c1a411845a516ea630
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-02-19 22:31:05 -0400

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { poweredBy } from "hono/powered-by";
 import { status } from "./handlers/status";
 import { auth } from "./handlers/auth";
@@ -8,6 +9,7 @@ const app = new Hono();
 
 app.use(poweredBy());
 app.use(timeout(30000));
+app.use("*", cors());
 
 // Health check
 app.get("/", (c) => {
